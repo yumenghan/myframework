0f0566018d0102b1756ea15794f98360ce2a4f85
yumenghan <y957088656@163.com>
2017-11-17 19:22:42 +0800

    add CollectionUtil

---
+++ src/main/java/com/myframework/util/CollectionUtil.java
@@ -0,0 +1,23 @@
+package com.myframework.util;
+
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.Collection;
+
+/**
+ * @author Menghan Yu
+ * @description 集合工具类
+ * @date 11/17/17 7:20 PM
+ */
+public final class CollectionUtil {
+    private CollectionUtil(){}
+
+    /**
+     * 判断集合是否为空
+     * @param collection
+     * @return
+     */
+    public static boolean isEmpty(Collection<?> collection) {
+        return CollectionUtils.isEmpty(collection);
+    }
+}
